d7103c70b1d86373d119ccf635e4d656aaf9ecd2
xwaynex <nobodysshinigami@proton.me>
2025-06-01 13:05:05 +0100

    -m moved cors check to server.js, -added swagger dir for all swagger related config

--- server.js
+++ server.js
@@ -7,38 +7,46 @@ const server = require('http').createServer(app)
 const cookieParser = require("cookie-parser");
 const WebSocket = require('ws')
 const wss = new WebSocket.Server({ server: server })  // use for WebSocket 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const db = require('./config/db')
-const PORT = process.env.PORT || 3500;
-
+const cors = require("cors");
+const { origins, methods } = require("./routes/allowedURLs");
 const { usersRoutes } = require("./routes/apiRouter");
+
+
+const PORT = process.env.PORT || 3500;
+const swaggerDocument = YAML.load(path.join(__dirname, './swagger/swagger.yaml'));
+
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Apply global CORS middleware once
+app.use(cors({
+  origin: origins,
+  methods,
+  credentials: true,
+}));
+
+// Swagger docs with HTTP-only cookie support
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  swaggerOptions: {
+    withCredentials: true,
+     requestInterceptor: req => {
+      req.credentials = 'include';
+      return req;
+    }
+  }
+}));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("An error have occurred!");
 });
 
-db.connectDB();
-
-
-// app.use("/", express.static(path.join(__dirname, "public")));
-// app.use("/", require("./routes/root"));
-
-// app.all("/*", (req, res) => {
-//   res.status(404);
-//   if (req.accepts("html")) {
-//     res.sendFile(path.join(__dirname, "views", "404.html"));
-//   } else if (req.accepts("json")) {
-//     res.json({ message: "404 Not Found" });
-//   } else {
-//     res.type("txt").send("404 Not Found");
-//   }
-// });
-
+db.connectDB()
 
 app.use("/api/user", usersRoutes);
 // add other routes here
