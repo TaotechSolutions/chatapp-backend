90942f546f4fc739c73a9bedfab08fb9110393ba
xwaynex <nobodysshinigami@proton.me>
2025-06-12 13:21:41 +0100

    -usercontroller cleanup

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,7 @@
-const UserServices = require("../services/UserServices");
 const { successResponse } = require("../utils/responses");
 
 class UserController {
-  static async getUserData(req, res, next) {
-    console.log(req.user);
+  static async getUserData(req, res) {
     return successResponse(res, 200, "User data fetched successfully", req.user);
   }
 }
