2cda4cc4f5d346d78c8b16dae8783692698befb1
xwaynex <103287435+xwaynex@users.noreply.github.com>
2025-06-12 15:39:08 +0100

    Oauth Improvement (#11)
    * oAuth fixes to integrate properly with FE
    
    * mustLogin refactor to pass oauth properly
    
    * return user details from mustBeLoggedIn remove second db call in user controller
    
    * -usercontroller cleanup

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,12 +1,23 @@
 const UserServices = require("../services/UserServices");
 const { errorResponse, successResponse } = require("../utils/responses");
-const { loginUserSchema, mongoIdSchema, forgotPasswordSchema, resetPasswordSchema } = require("../validators/auth");
-const { verifyHash, hashValue, cleanHash, isExpired, extendDate, compareDate } = require("../utils/helpers");
+const {
+  loginUserSchema,
+  mongoIdSchema,
+  forgotPasswordSchema,
+  resetPasswordSchema,
+} = require("../validators/auth");
+const {
+  verifyHash,
+  hashValue,
+  cleanHash,
+  isExpired,
+  extendDate,
+  compareDate,
+} = require("../utils/helpers");
 const { signJWTToken, verifyJWTToken } = require("../middlewares/TokenProvider");
 const EmailBluePrint = require("../utils/EmailBlueprint");
 const EmailServices = require("../services/EmailServices");
 const { JWT_SECRET, Api_consumer_URL, MAX_RESET_ATTEMPTS, RESET_TOKEN_EXPIRY } = process.env;
-
 
 const jwtCookieOptions = {
   httpOnly: true, // inaccessible to JavaScript (prevents XSS)
@@ -51,14 +62,13 @@ class AuthController {
       if (userStatus !== userSavedStatus) {
         return errorResponse(res, 403, "Your Account was deactivated. Please contact Support");
       } else {
-        req.apiUser = apiUser;
-        req.userId = apiUser._id;
+        req.user = user; //standardized
         req.userDetails = user;
         next();
       }
     } catch (error) {
       console.log(error);
-      next(error)
+      next(error);
     }
   }
 
@@ -111,7 +121,7 @@ class AuthController {
       await successResponse(res, 200, "Login successful", { user });
     } catch (error) {
       console.log(error);
-      next(error)
+      next(error);
     }
   }
 
@@ -139,16 +149,23 @@ class AuthController {
     if (!req.user || !req.user._id || !req.user.email) {
       return errorResponse(res, 400, "OAuth callback failed: User data missing.");
     }
-    const token = signJWTToken({ _id: req.user._id, email: req.user.email }, JWT_SECRET, "7d");
+    const token = signJWTToken(
+      { _id: req.user._id, email: req.user.email, status: req.user.status },
+      JWT_SECRET,
+      "7d"
+    );
 
     res.cookie("auth_token", token, jwtCookieOptions);
 
     // If request is from SPA expecting JSON (e.g., frontend redirect handler)
-    if (req.headers.accept?.includes("application/json")) {
-      return successResponse(res, 200, "OAuth login successful", req.user);
-    }
+    // if (req.headers.accept?.includes("application/json")) {
+    //   return successResponse(res, 200, "OAuth login successful", req.user);
+    // }
+
+    const redirectUrl = `${process.env.FRONTEND_REDIRECT_URL}/auth-callback`;
+
     // Otherwise, it's a traditional browser redirect flow
-    res.redirect(process.env.FRONTEND_REDIRECT_URL);
+    res.redirect(redirectUrl);
   }
 
   static async getResetPasswordLink(req, res, next) {
@@ -156,37 +173,58 @@ class AuthController {
       const { value, error } = forgotPasswordSchema.validate(req.body);
       if (error) return errorResponse(res, 400, error?.details[0]?.message);
       const { email } = value;
-      const userByEmail = await UserServices.findUserByData({ email: email.toLowerCase().trim() }, true)
-      if (!userByEmail) return errorResponse(res, 404, 'Email is not found.');
-      if (userByEmail?.oauthProvider !== "local") return errorResponse(res, 403, 'Password reset is unavailable as your account was created using Google Sign-In. Please use Google to log in.');
-      if (!userByEmail?.emailVerified) return errorResponse(res, 403, 'Verification failed, Email is not verified.');
+      const userByEmail = await UserServices.findUserByData(
+        { email: email.toLowerCase().trim() },
+        true
+      );
+      if (!userByEmail) return errorResponse(res, 404, "Email is not found.");
+      if (userByEmail?.oauthProvider !== "local")
+        return errorResponse(
+          res,
+          403,
+          "Password reset is unavailable as your account was created using Google Sign-In. Please use Google to log in."
+        );
+      if (!userByEmail?.emailVerified)
+        return errorResponse(res, 403, "Verification failed, Email is not verified.");
       const { _id, password, resetCount = 0, resetDate } = userByEmail;
       const isResetToday = compareDate(new Date(), resetDate);
-      if (resetCount >= Number(MAX_RESET_ATTEMPTS) && isResetToday) return errorResponse(res, 403, 'Password reset is unavailable as you have exceeded the maximum number of attempts. Try again after 24 hours.');
+      if (resetCount >= Number(MAX_RESET_ATTEMPTS) && isResetToday)
+        return errorResponse(
+          res,
+          403,
+          "Password reset is unavailable as you have exceeded the maximum number of attempts. Try again after 24 hours."
+        );
 
-      const secret = JWT_SECRET + password  // to make the reset url invalid after password change
-      const payload = { email: userByEmail.email, _id }
+      const secret = JWT_SECRET + password; // to make the reset url invalid after password change
+      const payload = { email: userByEmail.email, _id };
 
-      const token = await signJWTToken(payload, secret, RESET_TOKEN_EXPIRY)
-      let data = { resetToken: token, resetCount: resetCount + 1, resetDate: new Date() }
-      if (resetCount >= Number(MAX_RESET_ATTEMPTS) && !isResetToday) { data = { ...data, resetCount: 1 } }
-      await UserServices.updateUser(_id, data)
-      let hashVal = await hashValue(token)
-      hashVal = await cleanHash(hashVal, token)
-      const resetLink = `${Api_consumer_URL}/auth/reset-password/${_id}/${hashVal}`
+      const token = await signJWTToken(payload, secret, RESET_TOKEN_EXPIRY);
+      let data = { resetToken: token, resetCount: resetCount + 1, resetDate: new Date() };
+      if (resetCount >= Number(MAX_RESET_ATTEMPTS) && !isResetToday) {
+        data = { ...data, resetCount: 1 };
+      }
+      await UserServices.updateUser(_id, data);
+      let hashVal = await hashValue(token);
+      hashVal = await cleanHash(hashVal, token);
+      const resetLink = `${Api_consumer_URL}/auth/reset-password/${_id}/${hashVal}`;
       successResponse(res, 200, "Password reset link sent to your email address.");
 
       // Send user email
       const emailData = {
         email: userByEmail.email,
-        emailToBeSent, emailHead: "Reset Password",
-        emailSubject: "Reset password Request."
-      }
-      const emailToBeSent = EmailBluePrint.returnResetPasswordHTML(userByEmail, resetLink, "request") //return HTML email to be sent
-      await EmailServices.sendingEmailToUser(emailData)
+        emailToBeSent,
+        emailHead: "Reset Password",
+        emailSubject: "Reset password Request.",
+      };
+      const emailToBeSent = EmailBluePrint.returnResetPasswordHTML(
+        userByEmail,
+        resetLink,
+        "request"
+      ); //return HTML email to be sent
+      await EmailServices.sendingEmailToUser(emailData);
     } catch (error) {
-      console.log(error)
-      next(error)
+      console.log(error);
+      next(error);
     }
   }
 
@@ -195,28 +233,36 @@ class AuthController {
       const { value, error } = resetPasswordSchema.validate(req.body);
       if (error) return errorResponse(res, 400, error?.details[0]?.message);
       const { userId, password, token } = value;
-      const user = await UserServices.findUserByData({ _id: userId }, true)
-      if (!user) return errorResponse(res, 404, 'User not found.');
-      const isValid = await verifyHash(user.resetToken, token)
-      if (!isValid) { return errorResponse(res, 403, "Invalid reset link!") }
-      const secret = JWT_SECRET + user.password
-      const forgottenUser = await verifyJWTToken(user.resetToken, secret)
-      if (forgottenUser.status !== 200) return errorResponse(res, forgottenUser.status, forgottenUser.error);
-      const foundUserId = forgottenUser.result._id
-      const newPassword = await hashValue(password)
-      const updatedUser = await UserServices.updateUser(foundUserId, { password: newPassword, resetToken: "", resetCount: 0 })
+      const user = await UserServices.findUserByData({ _id: userId }, true);
+      if (!user) return errorResponse(res, 404, "User not found.");
+      const isValid = await verifyHash(user.resetToken, token);
+      if (!isValid) {
+        return errorResponse(res, 403, "Invalid reset link!");
+      }
+      const secret = JWT_SECRET + user.password;
+      const forgottenUser = await verifyJWTToken(user.resetToken, secret);
+      if (forgottenUser.status !== 200)
+        return errorResponse(res, forgottenUser.status, forgottenUser.error);
+      const foundUserId = forgottenUser.result._id;
+      const newPassword = await hashValue(password);
+      const updatedUser = await UserServices.updateUser(foundUserId, {
+        password: newPassword,
+        resetToken: "",
+        resetCount: 0,
+      });
       successResponse(res, 200, "Password reset successful.");
 
       // Send user email
       const emailData = {
         email: updatedUser.email,
-        emailToBeSent, emailHead: "Password Changed!",
-        emailSubject: "Your account password changed successfully."
-      }
-      const emailToBeSent = EmailBluePrint.returnResetPasswordHTML(updatedUser, "", "success") //return HTML email to be sent
-      await EmailServices.sendingEmailToUser(emailData)
+        emailToBeSent,
+        emailHead: "Password Changed!",
+        emailSubject: "Your account password changed successfully.",
+      };
+      const emailToBeSent = EmailBluePrint.returnResetPasswordHTML(updatedUser, "", "success"); //return HTML email to be sent
+      await EmailServices.sendingEmailToUser(emailData);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 }
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,15 +1,9 @@
-const UserServices = require('../services/UserServices');
+const { successResponse } = require("../utils/responses");
 
 class UserController {
-    static async getUserData(req, res, next) {
-        try {
-
-        } catch (error) {
-            console.log(error)
-            next(error)
-        }
-    }
+  static async getUserData(req, res) {
+    return successResponse(res, 200, "User data fetched successfully", req.user);
+  }
 }
-
 
 module.exports = UserController;
