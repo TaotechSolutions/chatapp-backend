34c38e46b6b42e263909710c06beaef5b79120cb
xwaynex <nobodysshinigami@proton.me>
2025-06-23 20:22:58 +0100

    new value add to login in user request payload, -using remember me value to make adjust cookie maxage

--- validators/auth.js
+++ validators/auth.js
@@ -1,33 +1,33 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const mongoIdSchema = Joi.object({
-    id: Joi.string().hex().length(24).required().messages({
-        'string.base': 'Invalid Id',
-        'string.hex': 'Invalid Id',
-        'string.length': 'Invalid Id',
-        'any.required': 'Invalid Id',
-    }),
-})
+  id: Joi.string().hex().length(24).required().messages({
+    "string.base": "Invalid Id",
+    "string.hex": "Invalid Id",
+    "string.length": "Invalid Id",
+    "any.required": "Invalid Id",
+  }),
+});
 
 const loginUserSchema = Joi.object({
-    email: Joi.string().email().required().trim(),
-    password: Joi.string().min(8).required()
+  email: Joi.string().email().required().trim(),
+  password: Joi.string().min(8).required(),
+  rememberMe: Joi.boolean().required(),
 });
 
 const forgotPasswordSchema = Joi.object({
-    email: Joi.string().email().required().trim()
+  email: Joi.string().email().required().trim(),
 });
 
 const resetPasswordSchema = Joi.object({
-    userId: Joi.string().hex().length(24).required().messages({
-        'string.base': 'Invalid User Id',
-        'string.hex': 'Invalid User Id',
-        'string.length': 'Invalid User Id',
-        'any.required': 'Invalid User Id',
-    }),
-    password: Joi.string().min(8).required(),
-    token: Joi.string().required()
+  userId: Joi.string().hex().length(24).required().messages({
+    "string.base": "Invalid User Id",
+    "string.hex": "Invalid User Id",
+    "string.length": "Invalid User Id",
+    "any.required": "Invalid User Id",
+  }),
+  password: Joi.string().min(8).required(),
+  token: Joi.string().required(),
 });
-
 
 module.exports = { loginUserSchema, mongoIdSchema, forgotPasswordSchema, resetPasswordSchema };
